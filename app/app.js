c6003a7b0957e50352e959309d5fa57f1ac5ba26
Rafal Otka <zooltar1@gmail.com>
2023-04-17 21:51:16 +0200

    Modified createBox function to count width and height of boxes inside container based on the input number

--- app/app.js
+++ app/app.js
@@ -4,10 +4,13 @@ function createBox(number) {
     for (let i = 0; i<number; i++) {
         const squareBox = document.createElement('div');
         squareBox.classList.add('square-box');
+        let square = Math.sqrt(number); // pierwiastek z liczby
+        squareBox.style.width = `calc(100% * (1 / ${square}) - 2px)`;
+        squareBox.style.height = `calc(100% * (1 / ${square}) - 2px)`;
         container.appendChild(squareBox); 
     }
 }
 
-createBox(256);
+createBox(49);
 
 
