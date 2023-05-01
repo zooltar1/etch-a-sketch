ab848a553a492c05925f188ffeb6c7327fb27cdc
Rafal Otka <zooltar1@gmail.com>
2023-05-01 10:34:09 +0200

    Added squareBox function to create boxes based on number, modified createBox function, all in app.js file

--- app/app.js
+++ app/app.js
@@ -1,26 +1,26 @@
 const button = document.querySelector('button.btn');
+const container = document.createElement('div');
+container.classList.add('container');
+document.body.appendChild(container);
+
 button.addEventListener('click', function(){
     squareNumber = prompt('Enter number of squares');
+    container.innerHTML = '';
     createBox(squareNumber);
 })
 
-function createBox(number) {
-    const container = document.createElement('div');
-    container.classList.add('container');
-    document.body.appendChild(container);
-    container.style.visibility = 'visible';
-    for (let i = 0; i<number; i++) {
-        const squareBox = document.createElement('div');
+function squareBox(number) {
+    const squareBox = document.createElement('div');
         squareBox.classList.add('square-box');
         let square = Math.sqrt(number);
         squareBox.style.width = `calc(100% * (1 / ${square}) - 2px)`;
         squareBox.style.height = `calc(100% * (1 / ${square}) - 2px)`;
         container.appendChild(squareBox);
-        squareBox.addEventListener('mouseover', function() {
-            squareBox.style.backgroundColor = 'green';
-        });
-    }
 }
 
-
-
+function createBox(number) {
+    container.style.visibility = 'visible';
+    for (let i = 0; i<number; i++) {
+        squareBox(number);
+    }
+}
